bd0bcd9a9a72878c2413065e634ec57658ceec9c
SimGoss <55884618+SimGoss@users.noreply.github.com>
2026-09-15 14:50:05 +0200

    Update make_a_table.py
    handling cases where no tags for HISAT2

--- inst/scripts/make_a_table.py
+++ inst/scripts/make_a_table.py
@@ -118,10 +118,13 @@ def discard_non_unique_mappings(bam_parser):
     for read in bam_parser:
         global n_bam_aligns
         n_bam_aligns += 1
-        NH_value = [t[2] for t in read[-1] if t[0] == 'NH'][0]  # Assumes the tag is always present
-        if NH_value == 1:
-            NM_value = [t[2] for t in read[-1] if t[0] == 'NM'][0]  # Assumes the tag is always present
-            yield (read[:-1] + (NM_value,))
+        try:
+            NH_value = [t[2] for t in read[-1] if t[0] == 'NH'][0]  # Assumes the tag is always present
+            if NH_value == 1:
+                NM_value = [t[2] for t in read[-1] if t[0] == 'NM'][0]  # Assumes the tag is always present
+                yield (read[:-1] + (NM_value,))
+        except (KeyError, IndexError, ValueError):
+            continue
 
 
 # Input: BAM iterator
